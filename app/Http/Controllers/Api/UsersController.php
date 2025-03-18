ab5304e99e849221b1adbc5f941b08bd9a65885a
Salmansha08 <salmanwiharja8@gmail.com>
2025-03-18 07:58:57 +0700

    fix: upload image

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -33,24 +33,26 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
-        $validated = $request->validate([
+        $data = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
             'picture' => 'nullable|image|mimes:jpg,png,jpeg|max:2048',
         ]);
 
-        $validated['password'] = Hash::make($validated['password']);
+        $data['password'] = Hash::make($data['password']);
+
+        $data = request()->only(['name', 'email', 'password']);
 
         if (request()->hasFile('picture')) {
             $file = $request->file('picture');
             $extension = $file->getClientOriginalExtension();
             $filename = time() . '_' . uniqid() . '.' . $extension;
-            $path = $file->storeAs('posts', $filename, 'public');
+            $path = $file->storeAs('users', $filename, 'public');
             $data['picture'] = '/storage/' . $path;
         }
 
-        $user = User::create($validated);
+        $user = User::create($data);
 
         return response()->json(['message' => 'User created successfully', 'user' => $user], 201);
     }
@@ -72,26 +74,28 @@ class UsersController extends Controller
     {
         $user = User::findOrFail($id);
 
-        $validated = $request->validate([
+        $request->validate([
             'name' => 'sometimes|string|max:255',
             'email' => ['sometimes', 'string', 'email', 'max:255', Rule::unique('users')->ignore($id)],
             'password' => 'sometimes|string|min:6',
             'picture' => 'nullable|image|mimes:jpg,png,jpeg|max:2048',
         ]);
 
-        if (isset($validated['password'])) {
-            $validated['password'] = Hash::make($validated['password']);
+        if (isset($request['password'])) {
+            $request['password'] = Hash::make($request['password']);
         }
+
+        $data = request()->only(['name', 'email', 'password']);
 
         if (request()->hasFile('picture')) {
             $file = $request->file('picture');
             $extension = $file->getClientOriginalExtension();
             $filename = time() . '_' . uniqid() . '.' . $extension;
-            $path = $file->storeAs('posts', $filename, 'public');
+            $path = $file->storeAs('users', $filename, 'public');
             $data['picture'] = '/storage/' . $path;
         }
 
-        $user->update($validated);
+        $user->update($data);
 
         return response()->json(['message' => 'User updated successfully', 'user' => $user]);
     }
